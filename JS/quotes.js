de153f2b6ae41acd32105612c6e5a3cd2dd9d0b2
pg803 <101824186+pg803@users.noreply.github.com>
2023-08-18 17:53:09 +0530

    Fix quotes

--- JS/quotes.js
+++ JS/quotes.js
@@ -1,4 +1,4 @@
-const api_url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
+const api_url = "https://api.quotable.io/random"
 const quote = document.getElementById('quote');
 const author = document.getElementById('author');
 
@@ -6,8 +6,8 @@ async function getquote(url) {
     const response = await fetch(url);
     var data = await response.json();
     console.log(data);
-    quote.innerHTML = data.quoteText;
-    author.innerHTML = data.quoteAuthor;
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
 }
 
 getquote(api_url)
