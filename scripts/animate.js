150e4f03eb2134d0fbe52d18a1a074e02756fdd9
pg803 <pg.7693@pm.me>
2024-04-27 12:28:33 +0530

    Update navbar a bit more

--- scripts/animate.js
+++ scripts/animate.js
@@ -10,3 +10,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+var win = window,
+    docEl = document.documentElement,
+    $logo = document.getElementById('logo-scroll');
+
+win.onscroll = function(){
+   var stop = (this.scrollY || docEl.scrollTop)  - (docEl.clientTop || 0);
+   $logo.style.backgroundColor =  stop > 100 ? "rgba(41, 39, 39, 0.5)!important;":"rgba(41, 39, 39, 0.9)" ;
+};
